e843ebaec84ec4f776939480c468ffea0625783c
Peeke Kuepers <pkuepers@kaliber.net>
2021-12-10 14:13:00 +0100

    Add example

--- example/config/default.js
+++ example/config/default.js
@@ -1,5 +1,22 @@
+const sanity = {
+  projectId: '5r7p2zew',
+  dataset: 'dev',
+  apiVersion: '2021-10-12',
+}
+
 module.exports = {
   kaliber: {
-    compileWithBabel: [/@kaliber\//]
+    compileWithBabel: [
+      /@kaliber\/use-observed-ref/,
+      /@kaliber\/use-element-size/,
+      /@kaliber\/sanity-image/
+    ]
+  },
+  client: {
+    sanity: {
+      ...sanity,
+      token: null,
+      useCdn: process.env.NODE_ENV === 'production'
+    }
   }
 }
--- example/src/index.html.js
+++ example/src/index.html.js
@@ -1,16 +1,21 @@
+import '/index.css'
+import '/reset.css'
+import config from '@kaliber/config'
 import App from '/App?universal'
 import javascript from '@kaliber/build/lib/javascript'
+import stylesheet from '@kaliber/build/lib/stylesheet'
 
 export default (
   <html lang='en'>
     <head>
       <meta charSet='utf-8' />
       <title>Library</title>
+      {stylesheet}
       <meta name='viewport' content='width=device-width, initial-scale=1' />
       {javascript}
     </head>
     <body>
-      <App />
+      <App config={config.client} />
     </body>
   </html>
 )
