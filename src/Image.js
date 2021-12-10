2be86658e9031420e59fb3b478764377860cf2a6
Peeke Kuepers <pkuepers@kaliber.net>
2021-12-10 09:13:24 +0100

    Refactor based on PR

--- src/Image.js
+++ src/Image.js
@@ -1,74 +1,98 @@
 import { useElementSize } from '@kaliber/use-element-size'
 import imageUrlBuilder from '@sanity/image-url'
 
-const SIZES = [320, 480, 720, 1024, 1440, 1920, 2760, 3600]
+const SIZES = [320, 480, 720, 1024, 1440, 1920, 2400, 3000, 3600]
 
-export function Image({ sanityConfig, image, layoutClassName = undefined, imgProps = {} }) {
-  const { ref: sizeRef, size: { width = 0 } } = useElementSize()
-  const { src, srcSet } = useSrcSet({ config: sanityConfig, image })
-
+export function Image({
+  sanityConfig,
+  image,
+  layoutClassName = undefined, 
+  imgProps = {} 
+}) {
   return (
-    <img
+    <ImageBase  
       {...imgProps}
-      ref={sizeRef}
-      className={layoutClassName}
-      sizes={Math.max(1, width) + 'px'}
-      alt={image.alt}
-      width={image.asset.metadata.dimensions.width}
-      height={image.asset.metadata.dimensions.height}
-      {...{ src, srcSet }}
+      {...{ sanityConfig, image, layoutClassName }}
+      adjustImage={adjustImageWidth()}
+      deriveSizes={deriveSizes()}
     />
   )
 }
 
-export function ImageFixedAspectRatio({ sanityConfig, image, aspectRatio, layoutClassName = undefined, imgProps = {} }) {
-  const { ref: sizeRef, size: { width = 0 } } = useElementSize()
-  const { src, srcSet } = useSrcSet({ config: sanityConfig, image, aspectRatio })
-
+export function ImageCropped({
+  sanityConfig,
+  image,
+  aspectRatio,
+  layoutClassName = undefined, 
+  imgProps = {} 
+}) {
   return (
-    <img
+    <ImageBase  
       {...imgProps}
-      ref={sizeRef}
-      className={layoutClassName}
-      sizes={Math.max(1, width) + 'px'}
-      alt={image.alt}
-      width={100}
-      height={100 / aspectRatio}
-      {...{ src, srcSet }}
+      {...{ sanityConfig, image, layoutClassName }}
+      adjustImage={adjustImageWidthAndCrop(aspectRatio)}
+      deriveSizes={deriveSizesCropped(aspectRatio)}
     />
   )
 }
 
-export function ImageCover({ sanityConfig, image, layoutClassName = undefined, imgProps = {} }) {
-  const { ref: sizeRef, size: { width = 0, height = 0 } } = useElementSize()
-  const { src, srcSet } = useSrcSet({ config: sanityConfig, image })
-
-  const dimensions = image.asset.metadata.dimensions
-  const actualWidth = (width && height) 
-    ? Math.max(height / dimensions.height * dimensions.width, width)
-    : 0
-
+export function ImageCover({
+  sanityConfig,
+  image,
+  aspectRatio,
+  layoutClassName = undefined, 
+  imgProps = {} 
+}) {
   return (
-    <img
+    <ImageBase  
       {...imgProps}
-      ref={sizeRef}
-      className={layoutClassName}
-      sizes={Math.max(1, actualWidth) + 'px'}
-      alt={image.alt}
-      width={dimensions.width}
-      height={dimensions.height}
+      {...{ sanityConfig, image, layoutClassName }}
+      adjustImage={adjustImageWidthAndCrop(aspectRatio)}
+      deriveSizes={deriveSizesCover(aspectRatio)}
       style={{
         objectFit: 'cover',
-        ...image.hotspot && { objectPosition: `${image.hotspot.x * 100}% ${image.hotspot.y * 100}%` }
+        ...image.hotspot && { 
+          // Because our cropped image size may not match the actual display size, 
+          // it's useful to set the object-position the hotspot x and y values
+          objectPosition: `${image.hotspot.x * 100}% ${image.hotspot.y * 100}%` 
+        }
       }}
-      {...{ src, srcSet }}
     />
   )
 }
 
+function ImageBase({
+  sanityConfig,
+  image,
+  adjustImage,
+  deriveSizes,
+  style = {}, 
+  imgProps = {}, 
+  layoutClassName = undefined 
+}) {
+  const { ref: sizeRef, size: displaySize } = useElementSize()
+  const { src, srcSet } = useSrcSet({ config: sanityConfig, image, adjustImage })
+  const { width, height, sizes } = useDerivedSizes({
+    deriveSizes, 
+    displaySize,
+    naturalSize: image.asset.metadata.dimensions  
+  })
 
-function useSrcSet({ config, image, aspectRatio = undefined }) {
+  return (
+    <img 
+      {...imgProps}
+      ref={sizeRef}
+      className={layoutClassName}
+      sizes={sizes + 'px'}
+      {...{ src, srcSet, width, height, style }}
+    />
+  )
+}
+
+function useSrcSet({ config, image, adjustImage }) {
   const builder = imageUrlBuilder(config)
+  const adjustImageRef = React.useRef(null)
+  adjustImageRef.current = adjustImage
 
   return React.useMemo(
     () => {
@@ -80,9 +104,8 @@ function useSrcSet({ config, image, aspectRatio = undefined }) {
         width: 1
       }
 
-      const sources = aspectRatio
-        ? SIZES.map(x => ({ src: builder.image(image).width(x).height(Math.round(x / aspectRatio)).quality(75).auto('format').url(), width: x }))
-        : SIZES.map(x => ({ src: builder.image(image).width(x).quality(75).auto('format').url(), width: x }))
+      const baseImage = builder.image(image).quality(80).auto('format')
+      const sources = SIZES.map(width => ({ src: adjustImageRef.current(baseImage, width).url(), width }))
 
       const src = sources.slice(-1)[0].src
       const srcSet = [thumb, ...sources].map(x => `${x.src} ${x.width}w`).join(',')
@@ -90,6 +113,55 @@ function useSrcSet({ config, image, aspectRatio = undefined }) {
 
       return { src, srcSet }
     },
-    [image, aspectRatio, builder]
+    [image, builder]
   )
+}
+
+function useDerivedSizes({ deriveSizes, naturalSize, displaySize }) {
+  const deriveSizesRef = React.useRef(null)
+  deriveSizesRef.current = deriveSizes
+
+  return React.useMemo(
+    () => deriveSizesRef.current({ naturalSize, displaySize }), 
+    [naturalSize.width, naturalSize.height, displaySize.width, displaySize.height]
+  )
+}
+
+function adjustImageWidth() {
+  return (image, width) => image.width(width)
+}
+
+function adjustImageWidthAndCrop(aspectRatio) {
+  return (image, width) => image.width(width).height(Math.round(width / aspectRatio)) 
+}
+
+function deriveSizes() {
+  return ({ naturalSize, displaySize }) => ({
+    width: naturalSize.width,
+    height: naturalSize.height,
+    sizes: displaySize.width + 'px'
+  })
+}
+
+function deriveSizesCropped(aspectRatio) {
+  return ({ naturalSize, displaySize }) => ({
+    width: naturalSize.width,
+    height: naturalSize.height / aspectRatio,
+    sizes: displaySize.width + 'px'
+  })
+}
+
+// deriveSizesCover can return a sizes value larger than the actual display 
+// width in case the image is scaled up by object-fit
+function deriveSizesCover(aspectRatio) {
+  return ({ naturalSize, displaySize }) => {
+    const sizes = (naturalSize.height && displaySize.height && displaySize.width) 
+    ? Math.max(displaySize.height / naturalSize.height * naturalSize.width, displaySize.width)
+    : 0
+    return {
+      width: naturalSize.width,
+      height: naturalSize.height / aspectRatio,
+      sizes: sizes + 'px'
+    }
+  }
 }
