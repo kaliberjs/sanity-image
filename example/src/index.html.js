d1bdf4bcf39f875173f574ab32c8aeb02ab0554c
Peeke Kuepers <pkuepers@kaliber.net>
2021-12-10 14:14:11 +0100

    Work through library checklist

--- example/src/index.html.js
+++ example/src/index.html.js
@@ -9,7 +9,7 @@ export default (
   <html lang='en'>
     <head>
       <meta charSet='utf-8' />
-      <title>Library</title>
+      <title>@kaliber/sanity-image</title>
       {stylesheet}
       <meta name='viewport' content='width=device-width, initial-scale=1' />
       {javascript}
