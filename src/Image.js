325293a3b701580823a96fb84703b12e66046291
Peeke Kuepers <pkuepers@kaliber.net>
2022-09-20 08:48:06 +0200

    Parse required dimension information from the asset ref, instead of getting it from the meta data

--- src/Image.js
+++ src/Image.js
@@ -1,17 +1,18 @@
 import { useElementSize } from '@kaliber/use-element-size'
 import imageUrlBuilder from '@sanity/image-url'
 
+const exampleAssetRef = 'image-3b90af5f4677efbc62374dcfda799040b699b4c5-4928x3280-jpg'
 const SIZES = [320, 480, 720, 1024, 1440, 1920, 2400, 3000, 3600]
 
 export function Image({
   sanityConfig,
   image,
   sizes = undefined,
-  layoutClassName = undefined, 
-  imgProps = {} 
+  layoutClassName = undefined,
+  imgProps = {}
 }) {
   return (
-    <ImageBase  
+    <ImageBase
       {...{ sanityConfig, image, sizes, layoutClassName, imgProps }}
       adjustImage={adjustImageWidth()}
       deriveSizes={deriveSizes()}
@@ -24,11 +25,11 @@ export function ImageCropped({
   image,
   aspectRatio,
   sizes = undefined,
-  layoutClassName = undefined, 
-  imgProps = {} 
+  layoutClassName = undefined,
+  imgProps = {}
 }) {
   return (
-    <ImageBase  
+    <ImageBase
       {...imgProps}
       {...{ sanityConfig, image, sizes, layoutClassName, imgProps }}
       adjustImage={adjustImageWidthAndCrop(aspectRatio)}
@@ -42,20 +43,20 @@ export function ImageCover({
   image,
   aspectRatio,
   sizes = undefined,
-  layoutClassName = undefined, 
-  imgProps = {} 
+  layoutClassName = undefined,
+  imgProps = {}
 }) {
   return (
-    <ImageBase  
+    <ImageBase
       {...{ sanityConfig, image, sizes, layoutClassName, imgProps }}
       adjustImage={adjustImageWidthAndCrop(aspectRatio)}
       deriveSizes={deriveSizesCover(aspectRatio)}
       style={{
         objectFit: 'cover',
-        ...image.hotspot && { 
+        ...image.hotspot && {
           // Because our cropped image size may not match the actual display size, 
           // it's useful to set the object-position the hotspot x and y values
-          objectPosition: `${image.hotspot.x * 100}% ${image.hotspot.y * 100}%` 
+          objectPosition: `${image.hotspot.x * 100}% ${image.hotspot.y * 100}%`
         }
       }}
     />
@@ -68,25 +69,21 @@ function ImageBase({
   adjustImage,
   deriveSizes,
   sizes = undefined,
-  style = {}, 
-  imgProps = {}, 
-  layoutClassName = undefined 
+  style = {},
+  imgProps = {},
+  layoutClassName = undefined
 }) {
-  if (image?.asset && !image.asset.metadata) {
-    throw new Error('Image asset doesn\'t have associated metadata. Did you forget to dereference the asset field (`image{..., asset->}`)?')
-  }
-
   const className = [imgProps.className, layoutClassName].filter(Boolean).join(' ')
   const { ref: sizeRef, size: displaySize } = useElementSize()
   const { src, srcSet } = useSrcSet({ config: sanityConfig, image, adjustImage })
   const { width, height, size } = useDerivedSizes({
     deriveSizes,
     displaySize,
-    naturalSize: image.asset?.metadata.dimensions ?? { width: 0, height: 0 }
+    naturalSize: parseDimensionsFromAssetRef(image.asset._ref ?? image.asset._id)
   })
 
   return (
-    <img 
+    <img
       {...imgProps}
       ref={sizeRef}
       sizes={sizes || `${size}px`}
@@ -102,15 +99,11 @@ function useSrcSet({ config, image, adjustImage }) {
 
   return React.useMemo(
     () => {
-      if (!image.asset) {
-        if (process.env.NODE_ENV !== 'production') console.warn('Image doesn\'t have associated asset object. This will render an <img /> tag with an empty src attribute.')
-        return { src: '', srcSet: '' }
-      }
-
+      const dimensions = parseDimensionsFromAssetRef(image.asset._ref ?? image.asset._id)
       const [maxSize] = SIZES.slice(-1)
       const sizes = SIZES.slice(0, -1)
-        .filter(w => w < image.asset.metadata.dimensions.width)
-        .concat(Math.min(image.asset.metadata.dimensions.width, maxSize))
+        .filter(w => w < dimensions.width)
+        .concat(Math.min(dimensions.width, maxSize))
 
       const thumb = {
         src: adjustImageRef.current(builder.image(image).quality(0).blur(20).auto('format'), 20).url(),
@@ -135,7 +128,7 @@ function useDerivedSizes({ deriveSizes, naturalSize, displaySize }) {
   deriveSizesRef.current = deriveSizes
 
   return React.useMemo(
-    () => deriveSizesRef.current({ naturalSize, displaySize }), 
+    () => deriveSizesRef.current({ naturalSize, displaySize }),
     [naturalSize.width, naturalSize.height, displaySize.width, displaySize.height]
   )
 }
@@ -145,7 +138,7 @@ function adjustImageWidth() {
 }
 
 function adjustImageWidthAndCrop(aspectRatio) {
-  return (image, width) => image.width(width).height(Math.round(width / aspectRatio)) 
+  return (image, width) => image.width(width).height(Math.round(width / aspectRatio))
 }
 
 function deriveSizes() {
@@ -168,7 +161,7 @@ function deriveSizesCropped(aspectRatio) {
 // width in case the image is scaled up by object-fit
 function deriveSizesCover(aspectRatio) {
   return ({ naturalSize, displaySize }) => {
-    const size = (displaySize.width && displaySize.height) 
+    const size = (displaySize.width && displaySize.height)
       ? Math.max(displaySize.height * aspectRatio, displaySize.width)
       : 0
 
@@ -178,4 +171,22 @@ function deriveSizesCover(aspectRatio) {
       size: Math.max(1, size)
     }
   }
+}
+
+function parseDimensionsFromAssetRef(ref) {
+  const [, id, dimensionString, format] = ref.split('-')
+
+  if (!id || !dimensionString || !format) throw malformedAssetRefError(ref)
+
+  const [imgWidthStr, imgHeightStr] = dimensionString.split('x')
+  const width = +imgWidthStr
+  const height = +imgHeightStr
+
+  if (!(isFinite(width) && isFinite(height))) throw malformedAssetRefError(ref)
+
+  return { width, height }
+}
+
+function malformedAssetRefError(ref) {
+  return new Error(`Malformed asset _ref ${ref}. Expected an id like ${exampleAssetRef}.`)
 }
