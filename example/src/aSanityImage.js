9c7482222d6824bfb2aacfca39cabf59d9a8fe20
Peeke Kuepers <pkuepers@kaliber.net>
2022-09-21 15:13:35 +0200

    Added image with dereferenced asset to assure backwards compatibility

--- example/src/aSanityImage.js
+++ example/src/aSanityImage.js
@@ -19,3 +19,102 @@ export const image = {
     'y': 0.23735709135853333
   }
 }
+
+export const imageWithDereferencedAsset = {
+  '_type': 'image',
+  'asset': {
+    '_createdAt': '2021-12-10T08:18:02Z',
+    '_id': 'image-3b90af5f4677efbc62374dcfda799040b699b4c5-4928x3280-jpg',
+    '_rev': 'SVWB3nzlMSR5ytjXeXWNgg',
+    '_type': 'sanity.imageAsset',
+    '_updatedAt': '2021-12-10T08:18:02Z',
+    'assetId': '3b90af5f4677efbc62374dcfda799040b699b4c5',
+    'extension': 'jpg',
+    'metadata': {
+      '_type': 'sanity.imageMetadata',
+      'blurHash': 'VTFsS*~qR4bIM{~q%MRPofofMxtRWXRjkCWBt8t8RjWB',
+      'dimensions': {
+        '_type': 'sanity.imageDimensions',
+        'aspectRatio': 1.502439024390244,
+        'height': 3280,
+        'width': 4928
+      },
+      'hasAlpha': false,
+      'isOpaque': true,
+      'lqip': 'data:image/jpeg;base64,/9j/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAANABQDASIAAhEBAxEB/8QAFwAAAwEAAAAAAAAAAAAAAAAAAAQGB//EACMQAAIBBAEDBQAAAAAAAAAAAAECAwAEBRESBhMhFjFBYYH/xAAWAQEBAQAAAAAAAAAAAAAAAAAEAQL/xAAaEQADAAMBAAAAAAAAAAAAAAAAAQISE0FR/9oADAMBAAIRAxEAPwDS3y1qLUNcRRPCRolkBFR2fxPTGYjeYY+z7i+Oajg2/wAqS9SXtpZCKXjcRMumV/n7qMuc1d7Zlbiu98QaXE+hab4I5XoWSfITvbRlYix4gsDRScuXuzISJXG/PvRU1Qbzpn//2Q==',
+      'palette': {
+        '_type': 'sanity.imagePalette',
+        'darkMuted': {
+          '_type': 'sanity.imagePaletteSwatch',
+          'background': '#2a3d54',
+          'foreground': '#fff',
+          'population': 8.57,
+          'title': '#fff'
+        },
+        'darkVibrant': {
+          '_type': 'sanity.imagePaletteSwatch',
+          'background': '#041c4e',
+          'foreground': '#fff',
+          'population': 0.02,
+          'title': '#fff'
+        },
+        'dominant': {
+          '_type': 'sanity.imagePaletteSwatch',
+          'background': '#596f86',
+          'foreground': '#fff',
+          'population': 8.68,
+          'title': '#fff'
+        },
+        'lightMuted': {
+          '_type': 'sanity.imagePaletteSwatch',
+          'background': '#adc1d5',
+          'foreground': '#000',
+          'population': 1.9,
+          'title': '#fff'
+        },
+        'lightVibrant': {
+          '_type': 'sanity.imagePaletteSwatch',
+          'background': '#a9c5e9',
+          'foreground': '#000',
+          'population': 0.32,
+          'title': '#fff'
+        },
+        'muted': {
+          '_type': 'sanity.imagePaletteSwatch',
+          'background': '#596f86',
+          'foreground': '#fff',
+          'population': 8.68,
+          'title': '#fff'
+        },
+        'vibrant': {
+          '_type': 'sanity.imagePaletteSwatch',
+          'background': '#6090ca',
+          'foreground': '#fff',
+          'population': 0.01,
+          'title': '#fff'
+        }
+      }
+    },
+    'mimeType': 'image/jpeg',
+    'originalFilename': 'nasa-yZygONrUBe8-unsplash.jpg',
+    'path': 'images/5r7p2zew/dev/3b90af5f4677efbc62374dcfda799040b699b4c5-4928x3280.jpg',
+    'sha1hash': '3b90af5f4677efbc62374dcfda799040b699b4c5',
+    'size': 1526320,
+    'uploadId': 'qnVFs1adMszmPFtbqvddEWggFFvd58wz',
+    'url': 'https://cdn.sanity.io/images/5r7p2zew/dev/3b90af5f4677efbc62374dcfda799040b699b4c5-4928x3280.jpg'
+  },
+  'crop': {
+    '_type': 'sanity.imageCrop',
+    'bottom': 0,
+    'left': 0,
+    'right': 0,
+    'top': 0
+  },
+  'hotspot': {
+    '_type': 'sanity.imageHotspot',
+    'height': 0.47471418271706667,
+    'width': 0.20547945205479445,
+    'x': 0.8972602739726028,
+    'y': 0.23735709135853333
+  }
+}
